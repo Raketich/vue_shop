654ef05b0f24c64c52be8a955514bc3dce18088c
Ramil Khamatshin <h.ramil001@gmail.com>
2020-09-16 16:27:38 +0500

    setup project

---
+++ src/components/VS-Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="vs-main"></div>
+</template>
+
+<script>
+export default {
+  name: "vs-main",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style scoped>
+.vs-main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 900px;
+  margin: 0 auto;
+}
+</style>
